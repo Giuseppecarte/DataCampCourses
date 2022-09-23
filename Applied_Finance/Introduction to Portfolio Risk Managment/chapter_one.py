47723ce9c08c7bcb33ea033c850a5d0c15ae0036
GiuseppeC <giuseppecarteg@hotmail.com>
2022-09-22 21:08:41 -0500

    Applied Finance Track

---
+++ Applied_Finance/Introduction to Portfolio Risk Managment/chapter_one.py
@@ -0,0 +1,13 @@
+'''
+        Course Overview
+
+Learn how to analyze investment return distributions, build portfolios and reduce risk 
+, and identify key factors which are driving portfolio returns.
+
+- Univariate Inverstment Risk 
+- Portfolio Inversting 
+- Factor Investing
+- Forecasting and Reducing Risk 
+'''
+
+
